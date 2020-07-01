4a4c1c154a6e4a637e7e975939ace9506b13f2ad
Matt Brogan <broganmatt11@gmail.com>
2020-07-01 11:07:55 -0400

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        public string name { get; set; }
+
+        private double balance;
+        public double Balance { get { return balance; } set { balance += value; } }
+
+        public double Deposit(double amount)
+        {
+            balance += amount;
+            return balance;
+        }
+        public double GetBalance()
+        {
+            return Balance += 1;
+        }
+
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount userBankAccount = new BankAccount();
+            Console.WriteLine("Please enter your name:");
+            userBankAccount.name = Console.ReadLine();
+            Console.WriteLine($"Hello, {userBankAccount.name} what can i assist you with today?");
+            string[] options = { "Get Balance", "Make a deposit" };
+            foreach(string choice in options)
+            {
+                Console.WriteLine(choice);
+            }
+            options[0] = "1";
+            options[1] = "2";
+            Console.WriteLine("Please select an option using 1 for get balance and 2 for deposit:");
+            var custChoice = Console.ReadLine();
+            if (custChoice == options[0])
+            {
+                Console.WriteLine(userBankAccount.GetBalance());
+            }
+            else if (custChoice == options[1])
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                var total = double.Parse(Console.ReadLine());
+                userBankAccount.Deposit(total);
+                Console.WriteLine($"After your deposit of {total}, your new balace is {userBankAccount.Balance}");
+                           
+            }
+            else
+            {
+                Console.WriteLine("Have a great day!");
+            }
+        }
+    }
+}
